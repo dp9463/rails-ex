8476710a0fa56f66035808bacf85468724232972
dpdpbj <dpdpbj@cn.ibm.com>
2020-02-24 00:48:59 -0800

    change rails env to development

--- openshift/remove.sh
+++ openshift/remove.sh
@@ -1,9 +1,11 @@
 oc delete pod/rails-postgresql-example-1-build \
           buildconfig.build.openshift.io/rails-postgresql-example \
           build.build.openshift.io/rails-postgresql-example-1 \
-          imagestream.image.openshift.io/rails-postgresql-exampl \
+          imagestream.image.openshift.io/rails-postgresql-example \
 	  replicationcontroller/postgresql-1e \
 	  deploymentconfig.apps.openshift.io/postgresql \
 	  deploymentconfig.apps.openshift.io/rails-postgresql-example \
   	  service/postgresql \
-	  service/rails-postgresql-exampl \
+	  service/rails-postgresql-example \
+	  route.route.openshift.io/rails-postgresql-example
+oc delete secret rails-postgresql-example
