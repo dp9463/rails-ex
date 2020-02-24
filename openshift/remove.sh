a3a124fe040f0dba066d25b51fe0f83ec46c1c89
dpdpbj <dpdpbj@cn.ibm.com>
2020-02-23 23:44:35 -0800

    precompile

--- openshift/remove.sh
+++ openshift/remove.sh
@@ -1,4 +1,9 @@
 oc delete pod/rails-postgresql-example-1-build \
           buildconfig.build.openshift.io/rails-postgresql-example \
           build.build.openshift.io/rails-postgresql-example-1 \
-          imagestream.image.openshift.io/rails-postgresql-example
+          imagestream.image.openshift.io/rails-postgresql-exampl \
+	  replicationcontroller/postgresql-1e \
+	  deploymentconfig.apps.openshift.io/postgresql \
+	  deploymentconfig.apps.openshift.io/rails-postgresql-example \
+  	  service/postgresql \
+	  service/rails-postgresql-exampl \
